c163748733ddd9f7e1c34476c66d2a64441a6f20
StivenChamorro <stivenchamorro493@gmail.com>
2024-05-29 11:50:58 -0500

    cambios

---
+++ app/Models/Truck.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Truck extends Model
+{
+    use HasFactory; 
+
+    public function truckers(){
+        return $this->belongsToMany('App\Models\Trucker');
+    }
+}
